da96ef18a57da4adaa5d6b8c39ccb22e84587619
Yin-Ai, Yuan <annie3383831@gmail.com>
2025-05-24 12:06:13 +0800

    Update 136_single_number.py

--- data structure/136_single_number.py
+++ data structure/136_single_number.py
@@ -1,2 +1,14 @@
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 # You must implement a solution with a linear runtime complexity and use only constant extra space.
+# Core logic: XOR & Hash set
+
+# 10011 & 11 ^= 10000
+# 10011 & 100 ^= 10111
+# 10011 & 10011 ^= 00000
+
+class Solution:
+  def single(self, nums: List[int]) -> int:
+    answer = 0
+    for num in nums:
+      answer ^= num
+    return answer
