82169ff26edf0ed3432ced178697f40818dbb26f
Yin-Ai, Yuan <annie3383831@gmail.com>
2025-05-25 11:09:01 +0800

    Update 14_longest_common_prefix.py

--- data structure/14_longest_common_prefix.py
+++ data structure/14_longest_common_prefix.py
@@ -1,1 +1,16 @@
+# String Compare Solution
 
+# Compare the word (i) of each string input
+# If found difference -> stop the loop and return the current same word
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+      result = ""
+      for i in range(len(strs)):
+        for s in strs:
+          if i = len(strs) or strs[i] != strs[0][i]:
+            return result
+        result += strs[0][i]
+      return result
+        
