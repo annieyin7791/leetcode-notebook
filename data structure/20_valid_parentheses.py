f716fe5f4efc4ea7610b8bb5dc442fe6c11d8c4b
Yin-Ai, Yuan <annie3383831@gmail.com>
2025-05-27 13:08:51 +0800

    Update 20_valid_parentheses.py

--- data structure/20_valid_parentheses.py
+++ data structure/20_valid_parentheses.py
@@ -1,1 +1,6 @@
+# A null stack [] stores pair records
 
+# Left parentheses -> add into stack[]
+# Right parentheses -> if match the left parentheses -> pop from stack[]
+# Right parentheses -> if not match -> push into stack[]
+# Finally, if stack[] is null, true
